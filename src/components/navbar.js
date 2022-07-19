f2e3db5e049eeef9a551f7536238e728cd5ef934
Yathish <yathish@socialsurvey.com>
2022-07-19 14:33:05 +0530

    2nd fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,4 @@
-// import logo from '../logo.png';
-// import {Link } from 'react-router-dom';
 
-// function Navbar() {
-
-//     return (
-//         <nav className="navbar">
-//              <a href="/"><img className="logo" src={logo} alt="" /></a>
-
-//              <ul className="navlist">
-//                 <li> <Link to="/services">Services</Link> </li>
-//                 <li> <Link to="/practices">Practices</Link> </li>
-//                <li> <Link to="/about">About Us</Link> </li>
-//             </ul>
-//             <a href="/call" className="callbutton1">Schedule a call</a>
-//         </nav>
-//     )
-// }
-
-// export default Navbar;
 import logo from "../logo.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -33,20 +14,27 @@ function Navbar() {
         </a>
       </div>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li className="nav-item">
-          <Link to="/services"  className="nav-links">Services</Link>
+      <li className="nav-item">
+          <Link to="/practices" className="nav-links">
+            Practices
+          </Link>{" "}
         </li>
         <li className="nav-item">
-          <Link to="/practices"  className="nav-links">Practices</Link>{" "}
+          <Link to="/services" className="nav-links">
+            Services
+          </Link>
         </li>
+        
         <li className="nav-item">
           {" "}
-          <Link to="/about"  className="nav-links">About Us</Link>{" "}
+          <Link to="/about" className="nav-links">
+            About Us
+          </Link>{" "}
         </li>
         <li className="nav-item">
-        <a href="/call" className="callbutton1">
-          Schedule a call
-        </a>
+          <a href="/call" className="callbutton1">
+            Schedule a call
+          </a>
         </li>
       </ul>
       <div className="nav-icon" onClick={handleClick}>
